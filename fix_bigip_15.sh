abc6fc308b86d06438bae123cea4f3388862d381
Qin Zhao <q.zhao@f5.com>
2021-03-03 18:18:23 +0800

    Improve fix_bigip_15.sh

--- fix_bigip_15.sh
+++ fix_bigip_15.sh
@@ -6,12 +6,12 @@ cat > /tmp/$$/alibaba-init.patch <<EOF
 @@ -22,6 +22,8 @@
  TMP_PUB_KEY_INDICES="/tmp/pub-key-indices"
  fetch_ssh_key=0
-
+ 
 +ALIBABA_FIRST_BOOT=0
 +
  ATTEMPTS=5
  FAILED=0
-
+ 
 @@ -68,6 +70,9 @@
      if [ \$DEBUG -ge 1 ]; then
          show_dhcp_settings
@@ -22,17 +22,16 @@ cat > /tmp/$$/alibaba-init.patch <<EOF
      # Remove the marker on 1st boot:
      rm -f \$VADC_FIRST_BOOT
      rc=\$?
-@@ -137,6 +142,16 @@
+@@ -137,6 +142,15 @@
      if [ \$rc -ne 0 ]; then ret_stat=\$RC_FAIL; fi
  fi
-
+ 
++ALIBABA_USERDATA_URL="http://100.100.100.200/latest/user-data"
 +if [ \$ALIBABA_FIRST_BOOT -eq 1 ] ; then
-+    if [ -x /etc/vadc-init/alibaba-userdata ] ; then
-+        log_echo "Execute customized userdata script."
-+        /etc/vadc-init/alibaba-userdata >/tmp/alibaba-userdata.log 2>&1 &
-+    elif [ -x /etc/vadc-init/default-alibaba-userdata ] ; then
-+        log_echo "Execute default userdata script."
-+        /etc/vadc-init/default-alibaba-userdata >/tmp/default-alibaba-userdata.log 2>&1 &
++    ALIBABA_USERDATA=\$(curl -m5 -sf \$ALIBABA_USERDATA_URL)
++    if [ ! -z \$ALIBABA_USERDATA ] && [ -x /etc/vadc-init/alibaba-setup ] ; then
++        log_echo "Execute alibaba setup script."
++        /etc/vadc-init/alibaba-setup >/var/log/alibaba-setup.log 2>&1 &
 +    fi
 +fi
 +
@@ -41,7 +40,7 @@ cat > /tmp/$$/alibaba-init.patch <<EOF
      dbg_print_delimiter
 EOF
 
-cat > /tmp/$$/default-alibaba-userdata <<EOF
+cat > /tmp/$$/alibaba-setup <<EOF
 #!/bin/bash -x
 
 ##########################################################################
@@ -70,42 +69,41 @@ function wait_until_mcpd_is_initialized() {
 ##########################################################################
 
 METADATA_URL="http://100.100.100.200/2016-01-01/meta-data"
-USERDATA_URL="http://100.100.100.200/2016-01-01/user-data"
-CURL="curl -s -m5"
-
-USERDATA=\$(\${CURL} \${USERDATA_URL})
-
-if [[ -n "\${USERDATA}" ]] ; then
-  echo "Customized userdata is input via API. Skip default userdata script."
-  exit 0
-fi
+CURL="curl -m5 -sf"
 
 wait_until_mcpd_is_initialized
 
 BIGIP_HOSTNAME=\$(\${CURL} \${METADATA_URL}/hostname)
 if [[ -n "\${BIGIP_HOSTNAME}" ]] ; then
+  echo "modify device name"
   if ! [[ "\${BIGIP_HOSTNAME}" == *.* ]] ; then
     BIGIP_HOSTNAME=\${BIGIP_HOSTNAME}.localdomain
   fi
   tmsh modify /sys global-settings hostname \${BIGIP_HOSTNAME}
+  tmsh mv cm device bigip1 \${BIGIP_HOSTNAME}
+  if [ -f /config/httpd/conf/ssl.key/server.key ] && [ -f /config/httpd/conf/ssl.crt/server.crt ] ; then
+    echo "renew device certificate"
+    openssl req -rand /dev/random -new -key /config/httpd/conf/ssl.key/server.key -x509 -days 3650 -out /config/httpd/conf/ssl.crt/server.crt \\
+      -subj "/C=--/ST=WA/L=Seattle/O=MyCompany/OU=MyOrg/CN=\${BIGIP_HOSTNAME}/emailAddress=root@\${BIGIP_HOSTNAME}" -extensions usr_cert
+  fi
 fi
 
 BIGIP_NTP=\$(\${CURL} \${METADATA_URL}/ntp-conf/ntp-servers | sed "s/\r$//g")
 if [[ -n "\${BIGIP_NTP}" ]] ; then
+  echo "add ntp servers"
   tmsh modify /sys ntp servers add { \${BIGIP_NTP} }
 fi
 
 BIGIP_DNS=\$(\${CURL} \${METADATA_URL}/dns-conf/nameservers | sed "s/\r$//g")
 if [[ -n "\${BIGIP_DNS}" ]] ; then
+  echo "add dns servers"
   tmsh modify /sys dns name-servers add { \${BIGIP_DNS} }
 fi
 
-tmsh modify /sys global-settings gui-setup disabled
 tmsh modify /sys db configsync.allowmanagement value enable
-tmsh modify /sys ntp timezone Asia/Shanghai
 tmsh save /sys config
-
-touch /tmp/default-alibaba-userdata.done
+tmsh restart /sys service httpd
+echo "complete to setup"
 EOF
 
 guestfish -a "$1" <<EOF
@@ -119,9 +117,9 @@ guestfish -a "$1" <<EOF
   download /etc/vadc-init/alibaba-init /tmp/$$/alibaba-init
   ! patch -s /tmp/$$/alibaba-init /tmp/$$/alibaba-init.patch
   upload /tmp/$$/alibaba-init /etc/vadc-init/alibaba-init
-  upload /tmp/$$/default-alibaba-userdata /etc/vadc-init/default-alibaba-userdata
-  chmod 0755 /etc/vadc-init/default-alibaba-userdata
-  selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/vadc-init/default-alibaba-userdata
+  upload /tmp/$$/alibaba-setup /etc/vadc-init/alibaba-setup
+  chmod 0755 /etc/vadc-init/alibaba-setup
+  selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/vadc-init/alibaba-setup
   sync
   exit
 EOF
